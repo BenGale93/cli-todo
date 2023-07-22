3d092fd9f243977c3c08aefe72354ab8423c0d65
Ben Gale <bengale2007@googlemail.com>
2023-07-22 11:43:08 +0100

    Changed list command behaviour
    Default is to not list done items to reduce clutter.
    Added an "all" flag which lists all todo items.

--- src/bin/todo/commands/list.rs
+++ src/bin/todo/commands/list.rs
@@ -1,14 +1,24 @@
 use chrono::NaiveDateTime;
 use clap::Args;
-use todo::prelude::*;
+use todo::prelude::{Status, *};
 
 use crate::commands::get_connection;
 
 #[derive(Args)]
-pub struct ListArgs {
+#[group(multiple = false)]
+struct StatusFilter {
+    /// Whether to display all items. By default, done items are hidden.
+    #[arg(short, long)]
+    all: bool,
     /// Status to filter the full list on.
     #[arg(short, long)]
     status: Option<Vec<Status>>,
+}
+
+#[derive(Args)]
+pub struct ListArgs {
+    #[command(flatten)]
+    status_filter: StatusFilter,
     /// Due date to filter on. Displays those due before the date given.
     #[arg(short, long)]
     due: Option<NaiveDateTime>,
@@ -17,7 +27,14 @@ pub struct ListArgs {
 impl ListArgs {
     pub fn run(self) -> Result<()> {
         let conn = get_connection()?;
-        let todos = list_todos(self.status, self.due, &conn)?;
+
+        let statuses = match (self.status_filter.all, self.status_filter.status) {
+            (false, None) => Some(vec![Status::ToDo, Status::InProgress]),
+            (true, _) => None,
+            (_, Some(s)) => Some(s),
+        };
+
+        let todos = list_todos(statuses, self.due, &conn)?;
         let todo_rows: Vec<ToDoRow> = todos.into_iter().map(|t| t.into()).collect();
         let table = tabled::Table::new(todo_rows);
         println!("{}", table);
